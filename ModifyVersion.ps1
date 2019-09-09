f374592ef1c48c3fd206f87358f9a3f503d95e3a
Riley White <rileywhite@gmail.com>
2019-09-08 20:41:25 -0500

    Move version pre-release indicator to csproj. Remove docgen for now.

--- ModifyVersion.ps1
+++ ModifyVersion.ps1
@@ -4,21 +4,22 @@ Param($projectFile, $buildNum)
 
 $content = [IO.File]::ReadAllText($projectFile)
 
-$regex = new-object System.Text.RegularExpressions.Regex ('()([\d]+.[\d]+.[\d]+)(.[\d]+)(<\/Version>)', 
+$regex = new-object System.Text.RegularExpressions.Regex ('()([\d]+.[\d]+.[\d]+)(.[\d]+)((-[a-zA-Z\.\-0-9]*)?<\/Version>)', 
          [System.Text.RegularExpressions.RegexOptions]::MultiLine)
 
 $version = $null
 $match = $regex.Match($content)
+echo $match
 if($match.Success) {
     # from "1.0.0.0" this will extract "1.0.0"
-    $version = $match.groups[2].value
+    $version = $match.groups[1].value
 }
 
 # suffix build number onto $version. eg "1.0.0.15"
 $version = "$version.$buildNum"
 
 # update "1.0.0.0" to "$version"
-$content = $regex.Replace($content, '${1}' + $version + '-dev${4}')
+$content = $regex.Replace($content, '${2}' + $version + '${4}')
 
 # update csproj file
 [IO.File]::WriteAllText($projectFile, $content)
